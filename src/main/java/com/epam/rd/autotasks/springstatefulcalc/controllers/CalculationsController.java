1d5af130a0f1547038f1889a36e8864b7ac00f9d
sanzhar baktybekov <baktybekov220402@gmail,com>
2024-08-13 01:29:10 +0600

    solved

--- src/main/java/com/epam/rd/autotasks/springstatefulcalc/controllers/CalculationsController.java
+++ src/main/java/com/epam/rd/autotasks/springstatefulcalc/controllers/CalculationsController.java
@@ -1,12 +1,11 @@
 package com.epam.rd.autotasks.springstatefulcalc.controllers;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @Controller
@@ -17,61 +16,95 @@ public class CalculationsController {
         this.requestSupervisor = requestSupervisor;
     }
 
+    private Cookie getCookie(String cookieCandidate) {
+        if (cookieCandidate.isEmpty()) {
+            cookieCandidate = UUID.randomUUID().toString();
+        }
+        Cookie cookie = new Cookie("serviceId", cookieCandidate);
+        cookie.setHttpOnly(true);
+        cookie.setPath("/");
+        return cookie;
+    }
+
+    private String buildSetCookieHeader(Cookie cookie) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(cookie.getName()).append("=").append(cookie.getValue()).append(";");
+        if (cookie.getMaxAge() > 0) {
+            sb.append(" Max-Age=").append(cookie.getMaxAge()).append(";");
+        }
+        if (cookie.getPath() != null) {
+            sb.append(" Path=").append(cookie.getPath()).append(";");
+        }
+        if (cookie.isHttpOnly()) {
+            sb.append(" HttpOnly;");
+        }
+        return sb.toString();
+    }
+
     @PutMapping("/calc/expression")
     public ResponseEntity<Void> setExpression(
             @RequestBody String expression,
-            HttpServletResponse response,
-            HttpServletRequest request
+            @CookieValue(value = "serviceId", defaultValue = "") String serviceId
     ) {
-        return ResponseEntity.status(
-                requestSupervisor.setExpression(expression, getCookie(request, response))
-        ).build();
+        Cookie cookie = getCookie(serviceId);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.SET_COOKIE, buildSetCookieHeader(cookie));
+        return ResponseEntity
+                .status(requestSupervisor.setExpression(expression, cookie.getValue()))
+                .headers(headers)
+                .build();
     }
-
-    private String getCookie(HttpServletRequest request, HttpServletResponse response) {
-        Cookie[] cookies = request.getCookies();
-        if(cookies != null) {
-            for(Cookie cookie : cookies) {
-                if (cookie.getName().equals("serviceId")) {
-                    return cookie.getValue();
-                }
-            }
-        }
-        String serviceId = UUID.randomUUID().toString();
-        response.addCookie(new Cookie("serviceId", serviceId));
-        return serviceId;
-    };
 
     @PutMapping("/calc/{key}")
     public ResponseEntity<Void> setVariable(
             @PathVariable String key,
             @RequestBody String value,
-            HttpServletRequest request,
-            HttpServletResponse response) {
-        return ResponseEntity.status(
-                requestSupervisor.setVariable(key, value, getCookie(request, response))
-        ).build();
+            @CookieValue(value = "serviceId", defaultValue = "") String serviceId
+    ) {
+        Cookie cookie = getCookie(serviceId);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.SET_COOKIE, buildSetCookieHeader(cookie));
+        return ResponseEntity
+                .status(requestSupervisor.setVariable(key, value, cookie.getValue()))
+                .headers(headers)
+                .build();
     }
 
     @DeleteMapping("/calc/expression")
-    public ResponseEntity<Void> deleteExpression(HttpServletRequest request, HttpServletResponse response) {
-        requestSupervisor.deleteExpression(getCookie(request, response));
-        return ResponseEntity.status(204).build();
+    public ResponseEntity<Void> deleteExpression(
+            @CookieValue(value = "serviceId", defaultValue = "") String serviceId
+    ) {
+        Cookie cookie = getCookie(serviceId);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.SET_COOKIE, buildSetCookieHeader(cookie));
+        requestSupervisor.deleteExpression(cookie.getValue());
+        return ResponseEntity.noContent().headers(headers).build();
     }
 
     @DeleteMapping("/calc/{key}")
-    public ResponseEntity<Void> deleteVariable(@PathVariable String key, HttpServletRequest request, HttpServletResponse response) {
-        requestSupervisor.deleteVariable(key, getCookie(request, response));
-        return ResponseEntity.status(204).build();
+    public ResponseEntity<Void> deleteVariable(
+            @PathVariable String key,
+            @CookieValue(value = "serviceId", defaultValue = "") String serviceId
+    ) {
+        Cookie cookie = getCookie(serviceId);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.SET_COOKIE, buildSetCookieHeader(cookie));
+        requestSupervisor.deleteVariable(key, cookie.getValue());
+        return ResponseEntity.noContent().headers(headers).build();
     }
 
     @GetMapping("/calc/result")
-    public ResponseEntity<Integer> getResult(HttpServletRequest request, HttpServletResponse response) {
+    public ResponseEntity<Integer> getResult(
+            @CookieValue(value = "serviceId", defaultValue = "") String serviceId
+    ) {
+        Cookie cookie = getCookie(serviceId);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.SET_COOKIE, buildSetCookieHeader(cookie));
         try {
-            int result = requestSupervisor.getResult(getCookie(request, response));
-            return ResponseEntity.ok(result);
+            int result = requestSupervisor.getResult(cookie.getValue());
+            return ResponseEntity.ok().headers(headers).body(result);
         } catch (Exception e) {
-            return ResponseEntity.status(409).build();
+            return ResponseEntity.status(409).headers(headers).build();
         }
     }
 }
